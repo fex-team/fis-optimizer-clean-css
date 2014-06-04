e2777059994076f4bc9303d9e550125d63f02a94
xiangshouding <fansekey@gmail.com>
2014-06-04 15:58:20 +0800

    升级clean-css 2.x，需要升级接口

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var CleanCss = require('clean-css');
 
 module.exports = function(content, file, conf){
     conf.processImport = false;
-    return CleanCss.process(content, conf);
+    return CleanCss(conf).minify(content);
 };
